b43a3072f4c091f7834a02121d10919878e3f990
BravinR <bravinrutoh@gmail.com>
2023-10-27 20:41:10 -0400

    Fixed the getEmailFromToken method

--- Backend/webSocketAPI/src/main/java/com/chatApp/webSocketAPI/Config/TokenProvider.java
+++ Backend/webSocketAPI/src/main/java/com/chatApp/webSocketAPI/Config/TokenProvider.java
@@ -2,7 +2,8 @@ package com.chatApp.webSocketAPI.Config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import static com.chatApp.webSocketAPI.Config.JwtConstant.SECRET_KEY;
 import static io.jsonwebtoken.security.Keys.hmacShaKeyFor;
 import static java.lang.String.valueOf;
 
+@Slf4j
 @Service
 public class TokenProvider {
 
@@ -21,16 +23,16 @@ public class TokenProvider {
     public String generateToken(Authentication authentication){
         String jwt = Jwts.builder().setIssuer("YipYap").setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + 600000))
                 .claim("Email", authentication.getName())
-                .signWith(key)
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
 
         return jwt;
     }
 
-    public String getEmailFromToken(String Token){
-        Token = Token.substring(7);
+    public String getEmailFromToken(String token){
+        String Token = token.substring(7);
         Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(Token).getBody();
-        String email = valueOf(claims.get("email"));
+        String email = valueOf(claims.get("Email"));
 
         return email;
     }
