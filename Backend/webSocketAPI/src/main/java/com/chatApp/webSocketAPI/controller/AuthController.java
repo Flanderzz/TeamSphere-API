f7da6c70a46c886eca09acc93b3389ea704f0b65
Devine Chinemere <74931751+Flanderzz@users.noreply.github.com>
2023-12-03 11:51:42 -0500

    added /signin endpoint

--- Backend/webSocketAPI/src/main/java/com/chatApp/webSocketAPI/controller/AuthController.java
+++ Backend/webSocketAPI/src/main/java/com/chatApp/webSocketAPI/controller/AuthController.java
@@ -65,6 +65,7 @@ public class AuthController {
         return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.ACCEPTED);
     }
 
+    @PostMapping("/signin")
     public ResponseEntity<AuthResponse> loginHandler(@RequestBody LoginRequest req){
         String email = req.getEmail();
         String password = req.getPassword();
