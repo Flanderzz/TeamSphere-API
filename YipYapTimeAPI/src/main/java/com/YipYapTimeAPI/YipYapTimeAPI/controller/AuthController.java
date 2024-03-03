4cafb42cadc103a556668bb09d91120d405abecf
bravin <bravinrutoh@gmail.com>
2024-03-03 16:50:22 -0500

    Removed wildcard imports

--- YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/controller/AuthController.java
+++ YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/controller/AuthController.java
@@ -20,7 +20,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 
