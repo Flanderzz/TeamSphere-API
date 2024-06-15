25eb383b2cd50c88cc07ce2d5bb9994172d583dd
bravin <bravinrutoh@gmail.com>
2024-06-15 10:10:39 -0400

    Add ordering of messages based on timestamp

--- YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/repository/MessageRepository.java
+++ YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/repository/MessageRepository.java
@@ -11,9 +11,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface MessageRepository  extends JpaRepository<Messages, Long> {
+public interface MessageRepository  extends JpaRepository<Messages, UUID> {
 
-    @Query("select m from Messages m join m.chat c where c.id=:chatId")
+    @Query("select m from Messages m join m.chat c where c.id=:chatId order by m.timeStamp asc")
     List<Messages> findMessageByChatId(@Param("chatId") UUID chatId);
 
     Optional<Messages> findById(UUID userId);
