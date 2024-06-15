dd73e5cd78043c7a19ac70f303ddf8d5973476de
bravin <bravinrutoh@gmail.com>
2024-06-15 19:47:21 -0400

    Added logging configurations

---
+++ YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.YipYapTimeAPI.YipYapTimeAPI.config;
+
+import com.YipYapTimeAPI.YipYapTimeAPI.Interceptors.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
