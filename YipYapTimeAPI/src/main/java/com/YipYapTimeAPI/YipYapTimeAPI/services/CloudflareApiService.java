f60bb5ad394855d291ad1dd81c86b9d4ab21e8af
Devine Chinemere <Fredfres1@gmail.com>
2024-12-28 22:26:19 -0500

    quick fixes

--- YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/services/CloudflareApiService.java
+++ YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/services/CloudflareApiService.java
@@ -1,13 +1,15 @@
 package com.YipYapTimeAPI.YipYapTimeAPI.services;
 
-import com.YipYapTimeAPI.YipYapTimeAPI.response.CloudflareApiResponse;
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import com.YipYapTimeAPI.YipYapTimeAPI.response.CloudflareApiResponse;
 
 @Service
 public interface CloudflareApiService {
     CloudflareApiResponse uploadImage(MultipartFile imageFile) throws IOException;
+    CloudflareApiResponse deleteImage(String imageID);
 }
 
