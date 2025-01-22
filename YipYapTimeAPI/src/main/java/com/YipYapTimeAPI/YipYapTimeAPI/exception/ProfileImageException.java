e60df6c746c05637fd5f75ee5b1c126985fc7109
bravin <bravinrutoh@gmail.com>
2025-01-21 22:04:47 -0500

    CHAT-48: to add a parent pom to allow addition of modules in future.

--- YipYapTimeAPI/src/main/java/com/YipYapTimeAPI/YipYapTimeAPI/exception/ProfileImageException.java
+++
@@ -1,7 +0,0 @@
-package com.YipYapTimeAPI.YipYapTimeAPI.exception;
-
-public class ProfileImageException extends Exception{
-    public ProfileImageException(String msg) {
-        super(msg);
-    }
-}
